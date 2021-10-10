488fd16379ef710a6d0a0bfc45420f5500c182a9
Dima-as <d.sichenka@gmail.com>
2021-10-11 00:37:49 +0300

    добавляет инлайн-стили на боди

---
+++ js/index.js
@@ -0,0 +1,51 @@
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  bodyEl: document.querySelector('body')
+};
+const { startBtn, stopBtn, bodyEl } = refs;
+
+startBtn.addEventListener('click', themeSwitcherClick);
+stopBtn.addEventListener('click' , stopThemeSwitcherClick);
+
+let intervalId = null
+let isActif = false
+
+function interval(){
+   if (isActif) {
+       return
+   }
+   isActif= true
+   intervalId = setInterval(() => {
+        const random = Math.floor(Math.random() * colors.length)
+        bodyEl.style.backgroundColor  = colors[random]
+        console.log(bodyEl);
+    }, 1000);
+}
+
+
+function themeSwitcherClick(){
+    interval()
+    return
+
+};
+
+ 
+function stopThemeSwitcherClick(){
+    clearInterval(intervalId)
+    isActif = false
+}
+
+const colors = [
+    "#FFFFFF",
+    "#2196F3",
+    "#4CAF50",
+    "#FF9800",
+    "#009688",
+    "#795548",
+  ];
+
+
+
+
